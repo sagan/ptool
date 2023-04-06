815a127dc2c49821c52012acde3477753e7e9ea3
Jacques De SAGAN <jacquesde@sagan.me>
2023-04-06 11:30:34 +0800

    status command now support multiple clients

--- cmd/status/status.go
+++ cmd/status/status.go
@@ -3,6 +3,7 @@ package status
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/sagan/ptool/client"
 	"github.com/sagan/ptool/cmd"
@@ -17,9 +18,9 @@ var (
 )
 
 var command = &cobra.Command{
-	Use:   "status <source>",
-	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
-	Short: "Show status",
+	Use:   "status ...clients",
+	Args:  cobra.MatchAll(cobra.MinimumNArgs(1), cobra.OnlyValidArgs),
+	Short: "Show clients status",
 	Long:  `A longer description`,
 	Run:   status,
 }
@@ -32,40 +33,59 @@ func init() {
 }
 
 func status(cmd *cobra.Command, args []string) {
-	client, err := client.CreateClient(args[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	status, err := client.GetStatus()
-	if err != nil {
-		log.Fatalf("Cann't get client %s status: error=%v", client.GetName(), err)
-	}
-	fmt.Printf("Client %s Download Speed/Limit: %s/s / %s/s; Upload Speed/Limit: %s/s / %s/s; Free disk space: %s\n\n",
-		client.GetName(),
-		utils.BytesSize(float64(status.DownloadSpeed)),
-		utils.BytesSize(float64(status.DownloadSpeedLimit)),
-		utils.BytesSize(float64(status.UploadSpeed)),
-		utils.BytesSize(float64(status.UploadSpeedLimit)),
-		utils.BytesSize(float64(status.FreeSpaceOnDisk)),
-	)
-
-	torrents, err := client.GetTorrents("", category, showAll)
-	if err != nil {
-		log.Fatal("!!", err)
-	}
-	fmt.Printf("Name  InfoHash  Tracker  State  DlSpeed  UpSpeed  Meta\n")
-	for _, torrent := range torrents {
-		if filter != "" && !utils.ContainsI(torrent.Name, filter) && !utils.ContainsI(torrent.InfoHash, filter) {
+	clientnames := args
+	isSingle := len(clientnames) == 1
+	hasError := false
+	for i, clientname := range clientnames {
+		if i > 0 {
+			fmt.Printf("\n")
+		}
+		client, err := client.CreateClient(clientname)
+		if err != nil {
+			log.Printf("Failed to create client %s: %v", clientname, err)
+			hasError = true
 			continue
 		}
-		fmt.Printf("%s  %s  %s  %s %s/s %s/s %v\n",
-			torrent.Name,
-			torrent.InfoHash,
-			torrent.TrackerDomain,
-			torrent.State,
-			utils.BytesSize(float64(torrent.DownloadSpeed)),
-			utils.BytesSize(float64(torrent.UploadSpeed)),
-			torrent.Meta,
+		status, err := client.GetStatus()
+		if err != nil {
+			log.Printf("Cann't get client %s status: error=%v", client.GetName(), err)
+			hasError = true
+			continue
+		}
+		fmt.Printf("Client %s Download Speed/Limit: %s/s / %s/s; Upload Speed/Limit: %s/s / %s/s; Free disk space: %s\n",
+			client.GetName(),
+			utils.BytesSize(float64(status.DownloadSpeed)),
+			utils.BytesSize(float64(status.DownloadSpeedLimit)),
+			utils.BytesSize(float64(status.UploadSpeed)),
+			utils.BytesSize(float64(status.UploadSpeedLimit)),
+			utils.BytesSize(float64(status.FreeSpaceOnDisk)),
 		)
+
+		if isSingle || showAll {
+			torrents, err := client.GetTorrents("", category, !isSingle && showAll)
+			if err != nil {
+				log.Printf("Cann't get client %s torrents: %v", clientname, err)
+				hasError = true
+				continue
+			}
+			fmt.Printf("\nName  InfoHash  Tracker  State  DlSpeed  UpSpeed  Meta\n")
+			for _, torrent := range torrents {
+				if filter != "" && !utils.ContainsI(torrent.Name, filter) && !utils.ContainsI(torrent.InfoHash, filter) {
+					continue
+				}
+				fmt.Printf("%s  %s  %s  %s %s/s %s/s %v\n",
+					torrent.Name,
+					torrent.InfoHash,
+					torrent.TrackerDomain,
+					torrent.State,
+					utils.BytesSize(float64(torrent.DownloadSpeed)),
+					utils.BytesSize(float64(torrent.UploadSpeed)),
+					torrent.Meta,
+				)
+			}
+		}
+	}
+	if hasError {
+		os.Exit(1)
 	}
 }
